6d18b03a3be06f1bfecb3352a995fc3ede56bd95
Giuseppe Desiato <giodesiato@gmail.com>
2023-08-24 07:19:20 -0400

    first occurence

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -19,6 +19,8 @@ public class BinarySearch {
         List<Integer> arrList = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
         int target = 6;
         System.out.println(firstNotSmaller(arrList, target));
+
+
     }
 
 
@@ -87,5 +89,27 @@ public class BinarySearch {
             }
         }
         return boundaryIndex;
+    }
+
+
+    // First Occurrence
+    public static int findFirstOccurrence(List<Integer> arr, int target) {
+        int left = 0;
+        int right = arr.size() -1;
+        int targetIndex = -1;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr.get(mid) == target){
+                targetIndex = mid;
+                right = mid - 1;
+            } else if (arr.get(mid) < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return targetIndex;
+
     }
 }
